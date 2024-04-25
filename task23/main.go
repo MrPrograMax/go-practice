94c4f138285925a85a4caa50b9abe5a37a64333a
MrPrograMax <grigorievma9461@gmail.com>
2024-04-25 23:27:27 +0700

    add task 23, 25 and gitignore

--- task23/main.go
+++ task23/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+func RemoveAt(arr []int, index int) ([]int, error) {
+	if index < 0 || len(arr) <= index {
+		return arr, errors.New("index out of range")
+	}
+
+	arr = append(arr[:index], arr[index+1:]...)
+	return arr, nil
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	index := 12
+	if arr, err := RemoveAt(arr, index); err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println(arr)
+	}
 }
--- task25/main.go
+++ task25/main.go
@@ -1,5 +1,17 @@
-package task25
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sleep(dur time.Duration) {
+	<-time.After(dur)
+}
 
 func main() {
+	fmt.Println("How long did it take?")
+	sleep(5 * time.Second)
+	fmt.Println("5 seconds...")
 
 }
